c15fa9c619064b793e5e83a1d32c4ced8e9157be
Julisnvv <91021791+Julisnvv@users.noreply.github.com>
2021-12-14 12:33:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 4200,
+    centerMode: true, 
+	autoHeight: true, 
+  });
+});
